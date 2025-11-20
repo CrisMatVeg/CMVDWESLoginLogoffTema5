3b22c590d86e8012c90e33314a5c572e3e86f441
cristian.matveg <cristian.matveg@educa.jcyl.es>
2025-11-20 10:00:55 +0100

    commit inicial

---
+++ config/confDBPDO.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+//DESARROLLO
+        define("HOST", "10.199.8.248");
+        define("DBNAME", "DBCMVDWESLoginLogoffTema5");
+        define("USERNAME", "userCMVDWESLoginLogoffTema5");
+        define("PASSWORD", "paso");
+        define("DSN", "mysql:host=" . HOST . "; dbname=" . DBNAME);
+//EXPLOTACION
+        /* define("HOST", "localhost");
+        define("DBNAME", "DBCMVDWESProyectoTema5");
+        define("USERNAME", "userCMVDWESProyectoTema5");
+        define("PASSWORD", "paso1234!");
+        define("DSN", "mysql:host=" . HOST . "; dbname=" . DBNAME); */
---
+++ scriptDB/BorraDBCMVDWESProyectoTema5EE.sql
@@ -0,0 +1,11 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  cristian.matveg
+ * Created: 30 oct. 2025
+ */
+
+DROP DATABASE IF EXISTS DBCMVDWESLoginLogoffTema5;
+DROP USER IF EXISTS userCMVDWESLoginLogoffTema5
